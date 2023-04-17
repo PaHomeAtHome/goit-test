73d4afef478b71c4aead58a82b108fdbda639642
Andrey Pakhomov <pahomeathome@gmail.com>
2023-04-17 23:09:32 +0300

    test

--- src/index.js
+++ src/index.js
@@ -2,36 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-import { Home } from './pages/Home/Home';
-import Error from './components/Error/Error';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Navigate,
-} from 'react-router-dom';
+// import { Home } from './pages/Home/Home';
+// import Error from './components/Error/Error';
+// // import {
+// //   createBrowserRouter,
+// //   RouterProvider,
+// //   Navigate,
+// // } from 'react-router-dom';
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Home />,
-      errorElement: <Error />,
-    },
-    {
-      path: '/tweets',
-      element: <App />,
-    },
-    {
-      path: '*',
-      element: <Navigate to="/" replace />,
-    },
-  ]
-  // { basename: "/tweets" }
-);
+// // const router = createBrowserRouter(
+// //   [
+// //     {
+// //       path: '/',
+// //       element: <Home />,
+// //       errorElement: <Error />,
+// //     },
+// //     {
+// //       path: '/tweets',
+// //       element: <App />,
+// //     },
+// //     {
+// //       path: '*',
+// //       element: <Navigate to="/" replace />,
+// //     },
+// //   ]
+// //   // { basename: "/tweets" }
+// // );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <App />
   </React.StrictMode>
 );
